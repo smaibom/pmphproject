ef4f3e2e975a45bfce3a94617f7c35888be765c6
William Sprent <bsprent@gmail.com>
2015-10-23 14:15:12 +0200

    Array expansion in rollback

--- src/ProjectPMPH/OrigImpl/ProjCoreOrig.cpp
+++ src/ProjectPMPH/OrigImpl/ProjCoreOrig.cpp
@@ -83,37 +83,37 @@ inline void tridag(
 void
 rollback( const unsigned g, PrivGlobs* globs, int outer, const int& numX,  const int& numY) {
 
+
+  unsigned numZ = max(numX,numY);
+
   vector<vector<vector<REAL> > > u(outer, vector<vector<REAL> > (numY, vector<REAL>(numX)));   // [numY][numX]
   vector<vector<vector<REAL> > > v(outer, vector<vector<REAL> > (numX, vector<REAL>(numY)));   // [numX][numY]
+
+  vector<vector<vector<REAL> > > ax(outer, vector<vector<REAL> > (numZ, vector<REAL>(numZ)));   // [max(numX,numY)][max(numX, numY)]
+  vector<vector<vector<REAL> > > bx(outer, vector<vector<REAL> > (numZ, vector<REAL>(numZ)));   // [max(numX,numY)][max(numX, numY)]
+  vector<vector<vector<REAL> > > cx(outer, vector<vector<REAL> > (numZ, vector<REAL>(numZ)));   // [max(numX,numY)][max(numX, numY)]
+  vector<vector<vector<REAL> > > ay(outer, vector<vector<REAL> > (numZ, vector<REAL>(numZ)));   // [max(numX,numY)][max(numX, numY)]
+  vector<vector<vector<REAL> > > by(outer, vector<vector<REAL> > (numZ, vector<REAL>(numZ)));   // [max(numX,numY)][max(numX, numY)]
+  vector<vector<vector<REAL> > > cy(outer, vector<vector<REAL> > (numZ, vector<REAL>(numZ)));   // [max(numX,numY)][max(numX, numY)]
+
+
+  vector<vector<vector<REAL> > > y(outer, vector<vector<REAL> > (numZ, vector<REAL>(numZ)));   // [max(numX,numY)][max(numX, numY)]
+  vector<vector<REAL> > yy(outer, vector<REAL>(numZ));  // temporary used in tridag  // [max(numX,numY)]
 #pragma omp parallel for default(shared) schedule(static) if(outer>8)
   for (int o = 0; o < outer; o++)
     {
-
-      unsigned numZ = max(numX,numY);
 
       unsigned i, j;
 
       REAL dtInv = 1.0/(globs[o].myTimeline[g+1]-globs[o].myTimeline[g]);
 
-
-      vector<vector<REAL> > ax(numZ, vector<REAL>(numZ));   // [max(numX,numY)][max(numX, numY)]
-      vector<vector<REAL> > bx(numZ, vector<REAL>(numZ));   // [max(numX,numY)][max(numX, numY)]
-      vector<vector<REAL> > cx(numZ, vector<REAL>(numZ));   // [max(numX,numY)][max(numX, numY)]
-
-      vector<vector<REAL> > ay(numZ, vector<REAL>(numZ));   // [max(numX,numY)][max(numX, numY)]
-      vector<vector<REAL> > by(numZ, vector<REAL>(numZ));   // [max(numX,numY)][max(numX, numY)]
-      vector<vector<REAL> > cy(numZ, vector<REAL>(numZ));   // [max(numX,numY)][max(numX, numY)]
-
-      vector<vector<REAL> > y(numZ, vector<REAL>(numZ));   // [max(numX,numY)][max(numX, numY)]
-      vector<REAL> yy(numZ);  // temporary used in tridag  // [max(numX,numY)]
-
       // X-loop
       for(j=0;j<numY;j++) {
         for(i=0;i<numX;i++) {
           // implicit x
-          ax[j][i] =		 - 0.5*(0.5*globs[o].myVarX[i][j]*globs[o].myDxx[i][0]);
-          bx[j][i] = dtInv - 0.5*(0.5*globs[o].myVarX[i][j]*globs[o].myDxx[i][1]);
-          cx[j][i] =		 - 0.5*(0.5*globs[o].myVarX[i][j]*globs[o].myDxx[i][2]);
+          ax[o][j][i] =		 - 0.5*(0.5*globs[o].myVarX[i][j]*globs[o].myDxx[i][0]);
+          bx[o][j][i] = dtInv - 0.5*(0.5*globs[o].myVarX[i][j]*globs[o].myDxx[i][1]);
+          cx[o][j][i] =		 - 0.5*(0.5*globs[o].myVarX[i][j]*globs[o].myDxx[i][2]);
 
 
           //	explicit x
@@ -152,58 +152,32 @@ rollback( const unsigned g, PrivGlobs* globs, int outer, const int& numX,  const
             u[o][j][i] += v[o][i][j];
 
             // Implicit y
-            ay[i][j] =		 - 0.5*(0.5*globs[o].myVarY[i][j]*globs[o].myDyy[j][0]);
-            by[i][j] = dtInv - 0.5*(0.5*globs[o].myVarY[i][j]*globs[o].myDyy[j][1]);
-            cy[i][j] =		 - 0.5*(0.5*globs[o].myVarY[i][j]*globs[o].myDyy[j][2]);
+            ay[o][i][j] =		 - 0.5*(0.5*globs[o].myVarY[i][j]*globs[o].myDyy[j][0]);
+            by[o][i][j] = dtInv - 0.5*(0.5*globs[o].myVarY[i][j]*globs[o].myDyy[j][1]);
+            cy[o][i][j] =		 - 0.5*(0.5*globs[o].myVarY[i][j]*globs[o].myDyy[j][2]);
 
           }
       }
 
       for(j=0;j<numY;j++) {
         // here yy should have size [numX]
-        tridag(ax[j],bx[j],cx[j],u[o][j],numX,u[o][j],yy);
+        tridag(ax[o][j],bx[o][j],cx[o][j],u[o][j],numX,u[o][j], yy[o]);
       }
 
       //	implicit y
       for(i=0;i<numX;i++) {
         for(j=0;j<numY;j++) {  // here a, b, c should have size [numY]
-          y[i][j] = dtInv*u[o][j][i] - 0.5*v[o][i][j];
+          y[o][i][j] = dtInv*u[o][j][i] - 0.5*v[o][i][j];
         }
       }
 
       for(i=0;i<numX;i++) {
         // here yy should have size [numY]
-        tridag(ay[i],by[i],cy[i],y[i],numY,globs[o].myResult[i],yy);
+        tridag(ay[o][i],by[o][i],cy[o][i],y[o][i],numY,globs[o].myResult[i],yy[o]);
       }
     }
 }
 
-// REAL   value(   PrivGlobs    globs,
-//                 const REAL s0,
-//                 const REAL strike,
-//                 const REAL t,
-//                 const REAL alpha,
-//                 const REAL nu,
-//                 const REAL beta,
-//                 const unsigned int numX,
-//                 const unsigned int numY,
-//                 const unsigned int numT
-// ) {
-//     initGrid(s0,alpha,nu,t, numX, numY, numT, globs);
-//     initOperator(globs.myX,globs.myDxx);
-//     initOperator(globs.myY,globs.myDyy);
-
-//     setPayoff(strike, globs);
-
-//   for(int i = numT-2;i>=0;--i)
-//    {
-//      updateParams(i,alpha,beta,nu,globs);
-//      rollback(i, globs);
-//    }
-
-
-//     return globs.myResult[globs.myXindex][globs.myYindex];
-// }
 
 void   run_OrigCPU(
                 const unsigned int&   outer,
