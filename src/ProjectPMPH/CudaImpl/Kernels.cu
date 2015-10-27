8659829bdb68ab12a5498a28cb99f0b5376f850f
simon <smaibom@gmail.com>
2015-10-27 16:43:38 +0100

    trying

---
+++ src/ProjectPMPH/CudaImpl/Kernels.cu
@@ -0,0 +1,12 @@
+
+__global__ void updateParamsKernel(const unsigned g, const REAL alpha, 
+                                   const REAL beta, const REAL nu, REAL* myVarX
+                                   REAL* myVarY, REAL* myY, REAL* myX, 
+                                   REAL* myTimeline,const int numY, const int numM){
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    int z = blockIdx.z * blockDim.z + threadIdx.z;
+
+    myVarX[z*numM+i*numY+j] = exp(2.0*(beta*log(myX[i])+myY[j]-0.5*nu*nu*myTimeline[g]));
+
+}
